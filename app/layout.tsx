172f846a26f62760196175e8fdc782b02d4a9e11
adammorris <adammorris1213@gmail.com>
2025-12-05 11:46:57 +0200

    Add Google site verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="6offM69pD82oFOX5xZB2fp8Mb9TZuU9v-1-_5wwJbqU" />
+      </head>
       <body className={`${heading.variable} ${body.variable} antialiased`}>
         {children}
       </body>
